499da64098c405aaa9b7f9fd4db207a7dd4a56c5
barsoosayque <shtoshich@gmail.com>
2018-12-21 13:16:07 +0700

    Split AudioDecoder::decode into two methods

--- library/src/kotlin/barsoosayque/libgdxoboe/AudioDecoder.kt
+++ library/src/kotlin/barsoosayque/libgdxoboe/AudioDecoder.kt
@@ -6,6 +6,7 @@ import android.media.MediaExtractor
 import android.media.MediaFormat
 import android.util.Log
 import java.io.ByteArrayOutputStream
+import kotlin.math.min
 
 
 /** Decoder class which can process audio files to PCM.
@@ -27,56 +28,78 @@ class AudioDecoder(fd: AssetFileDescriptor) {
             }
         } else null
     } ?: throw IllegalArgumentException("Can't extract audio from \"$fd\".")
+    private val info = MediaCodec.BufferInfo()
 
-    /** Read more data and decode it.
-     * @param samples Amount of new samples to be decoded */
-    fun decode(samples: Int): ByteArray {
-        TODO()
+    private fun readSampleData(): Boolean? =
+            decoder.dequeueInputBuffer(-1).takeIf { it >= 0 }?.let { bufferIndex ->
+                extractor.readSampleData(decoder.inputBuffers[bufferIndex], 0).let { size ->
+                    if(size > 0) {
+                        decoder.queueInputBuffer(bufferIndex, 0, size, extractor.sampleTime, 0)
+                        extractor.advance()
+                        false
+                    } else {
+                        decoder.queueInputBuffer(bufferIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM)
+                        true
+                    }
+                }
+            }
+
+    private fun parseSamples(destination: ByteArrayOutputStream, maximumBytes: Int? = null): Int {
+        var bytesProcessed = 0
+        decoder.dequeueOutputBuffer(info, -1).takeIf { it >= 0 }?.also { bufferIndex ->
+            when (bufferIndex) {
+                MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED -> Log.d("AudioDecoder", "Buffers changed.")
+                MediaCodec.INFO_OUTPUT_FORMAT_CHANGED -> Log.d("AudioDecoder", "Format changed.")
+                MediaCodec.INFO_TRY_AGAIN_LATER -> Log.d("AudioDecoder", "Timeout. Trying again...")
+                else -> {
+                    decoder.outputBuffers[bufferIndex].also { buffer ->
+                        Log.d("AudioDecoder", "Actually writing to the stream ${info.size}")
+                        buffer.position(info.offset)
+                        buffer.limit(info.offset + (maximumBytes?.let { min(info.size, it) } ?: info.size))
+                        val tmp = ByteArray(buffer.remaining())
+                        buffer.get(tmp)
+                        destination.write(tmp)
+                        bytesProcessed = tmp.size
+                    }
+                    decoder.releaseOutputBuffer(bufferIndex, false)
+                }
+            }
+        }
+
+        return bytesProcessed
     }
 
-    /** Read file to EOF and decode it all. */
-    fun decode(): ByteArray {
+
+    /** Read more data and decode it.
+     * @param samples Amount of new samples to be decoded
+     * (negative value to read and decode to eof) */
+    fun decode(samples: Int): ByteArray {
+        Log.d("AudioDecoder", "Samples requested: $samples")
         val stream = ByteArrayOutputStream()
-        val info = MediaCodec.BufferInfo()
+        var bytesLeft = samples * 2
         var eofExtractor = false
         var eofDecoder = false
 
         decoder.start()
-        while (!eofExtractor && !eofDecoder) {
-            decoder.dequeueInputBuffer(-1).takeIf { it >= 0 }?.also { bufferIndex ->
-                extractor.readSampleData(decoder.inputBuffers[bufferIndex], 0).also { size ->
-                    eofExtractor = !extractor.advance()
-                    if (eofExtractor) {
-                        decoder.queueInputBuffer(bufferIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM)
-                    } else {
-                        decoder.queueInputBuffer(bufferIndex, 0, size, extractor.sampleTime, 0)
-                    }
-                }
+        while (!(eofExtractor && eofDecoder) && bytesLeft != 0) {
+            Log.d("AudioDecoder", "Bytes Left: $bytesLeft (eofe $eofExtractor | eofd $eofDecoder)")
+            if (!eofExtractor) {
+                eofExtractor = readSampleData() ?: eofExtractor
             }
 
-            eofDecoder = info.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM != 0
-            decoder.dequeueOutputBuffer(info, -1).takeIf { it >= 0 }?.also { bufferIndex ->
-                when(bufferIndex) {
-                    MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED -> Log.d("AudioDecoder", "Buffers changed.")
-                    MediaCodec.INFO_OUTPUT_FORMAT_CHANGED -> Log.d("AudioDecoder", "Format changed.")
-                    MediaCodec.INFO_TRY_AGAIN_LATER -> Log.d("AudioDecoder", "Timeout. Trying again...")
-                    else -> if(!eofDecoder) {
-                        decoder.outputBuffers[bufferIndex].also { buffer ->
-                            buffer.position(info.offset)
-                            buffer.limit(info.offset + info.size)
-                            val tmp = ByteArray(buffer.remaining())
-                            buffer.get(tmp)
-                            stream.write(tmp)
-                        }
-                        decoder.releaseOutputBuffer(bufferIndex, false)
-                    }
-                }
+            eofDecoder = eofDecoder || info.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM != 0
+            if (!eofDecoder) {
+                bytesLeft -= parseSamples(stream, bytesLeft.takeIf { it > 0 })
             }
         }
         decoder.stop()
 
+        Log.d("AudioDecoder", "Stream size: ${stream.size()}")
         return stream.toByteArray()
     }
+
+    /** Read file to EOF and decode it all. */
+    fun decode(): ByteArray = decode(-1)
 
     /** Move head of the decoder to the @position (in seconds) */
     fun seek(position: Float) {
