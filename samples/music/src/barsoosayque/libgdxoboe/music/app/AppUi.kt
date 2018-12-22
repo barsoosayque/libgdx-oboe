028511647a354e5bb92363f5907999c65b2db6a4
barsoosayque <shtoshich@gmail.com>
2018-12-22 20:47:14 +0700

    Position and duration preview

--- samples/music/src/barsoosayque/libgdxoboe/music/app/AppUi.kt
+++ samples/music/src/barsoosayque/libgdxoboe/music/app/AppUi.kt
@@ -4,11 +4,14 @@ import barsoosayque.libgdxoboe.music.content.MusicAsset
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.audio.Music
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar
 import com.badlogic.gdx.utils.viewport.ExtendViewport
 import ktx.actors.onChange
 import ktx.actors.onChangeEvent
+import ktx.actors.txt
 import ktx.scene2d.*
+import kotlin.math.floor
 import com.badlogic.gdx.utils.Array as GdxArray
 
 class AppUi(
@@ -16,7 +19,9 @@ class AppUi(
         assetManager: AssetManager
 ) : Stage(ExtendViewport(700f, 480f)) {
     private lateinit var positionProgressBar: ProgressBar
-    private var selectedMusic: Music = music.first().get(assetManager)
+    private lateinit var positionLabel: Label
+    private var selectedAsset: MusicAsset = music.first()
+    private var selectedMusic: Music = selectedAsset.get(assetManager)
 
     val root = table {
         setFillParent(true)
@@ -29,7 +34,9 @@ class AppUi(
             label("Controls")
             row()
             listWidgetOf(GdxArray(music)).cell(growY = true).onChangeEvent { _, list ->
-                selectedMusic = list.selected.get(assetManager)
+                selectedMusic.stop()
+                selectedAsset = list.selected
+                selectedMusic = selectedAsset.get(assetManager)
             }
             verticalGroup {
                 horizontalGroup {
@@ -41,10 +48,25 @@ class AppUi(
                 }
             }.cell(growX = true)
             row()
-            container {
-                width(600f)
-                positionProgressBar = progressBar()
+            horizontalGroup {
+                container {
+                    width(600f)
+                    positionProgressBar = progressBar()
+                }
+                positionLabel = label("0:00 / 0:00")
             }.cell(colspan = 2)
         }.cell(grow = true)
     }.let(::addActor)
+
+    override fun act(delta: Float) {
+        super.act(delta)
+        positionProgressBar.value = selectedMusic.position / selectedAsset.duration
+        positionLabel.txt = "${selectedMusic.position.time()} / ${selectedAsset.duration.time()}"
+    }
+
+    private fun Float.time(): String {
+        val minutes = floor(this / 60)
+        val seconds = this - minutes * 60
+        return "${minutes.toInt()}:${seconds.toInt()}"
+    }
 }
--- samples/music/src/barsoosayque/libgdxoboe/music/content/Assets.kt
+++ samples/music/src/barsoosayque/libgdxoboe/music/content/Assets.kt
@@ -4,9 +4,9 @@ import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.audio.Music
 
 enum class SkinAsset(val path: String) { DEFAULT("uiskin.json") }
-enum class MusicAsset(val path: String) {
-    PEACH_PIT("peach_pit.ogg"),
-    OEDO_CONTROLLER("oedo_controller.mp3");
+enum class MusicAsset(val path: String, val duration: Float) {
+    PEACH_PIT("peach_pit.ogg", 287f),
+    OEDO_CONTROLLER("oedo_controller.mp3", 255.6f);
 
     fun get(assetManager: AssetManager): Music = assetManager[path]
     override fun toString(): String = "${super.toString()} ($path)"
