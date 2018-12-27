963a8fc70db922f12f5d6a541af829e378c8f7dc
barsoosayque <shtoshich@gmail.com>
2018-12-27 18:34:31 +0700

    Class which encapsulate work with libsamplerate

---
+++ library/src/cpp/samplerate/resampler.cpp
@@ -0,0 +1,74 @@
+#include "resampler.hpp"
+#include "../utility/log.hpp"
+
+resampler::resampler(resampler::converter p_converter, int8_t p_channels, float p_ratio)
+    : m_state(src_new(static_cast<int>(p_converter), p_channels, &m_error))
+    , m_converter(p_converter)
+    , m_channels(p_channels) {
+
+    m_data = new SRC_DATA {
+        .data_in = nullptr,
+        .data_out = nullptr,
+        .input_frames = 0,
+        .output_frames = 0,
+        .input_frames_used = 0,
+        .output_frames_gen = 0,
+        .end_of_input = 1,
+        .src_ratio = 1.0
+    };
+
+    ratio(p_ratio);
+}
+
+resampler::resampler(const resampler& p_instance)
+    : m_state(src_new(static_cast<int>(p_instance.m_converter), p_instance.m_channels, &m_error))
+    , m_channels(p_instance.m_channels) {
+    m_data = new SRC_DATA;
+    *m_data = *p_instance.m_data;
+    ratio(p_instance.m_data->src_ratio);
+}
+
+float resampler::ratio() {
+    return static_cast<float>(m_data->src_ratio);
+}
+
+void resampler::check_error() {
+    if (m_error != 0) {
+        error("resampler error: {}", src_strerror(m_error));
+        m_error = 0;
+    }
+}
+
+void resampler::ratio(float p_ratio) {
+    m_data->src_ratio = p_ratio;
+}
+
+resampler::~resampler() {
+    src_delete(m_state);
+    delete m_data;
+}
+
+std::vector<int16_t> resampler::process(std::vector<int16_t>::iterator p_begin,
+                                        std::vector<int16_t>::iterator p_end,
+                                        bool p_last) {
+    int len = std::distance(p_begin, p_end);
+    std::vector<float> float_buf(len),
+                       float_out(std::ceil(len * m_data->src_ratio));
+
+    src_short_to_float_array(&(*p_begin), float_buf.data(), len);
+
+    m_data->data_in = float_buf.data();
+    m_data->data_out = float_out.data();
+    m_data->input_frames = float_buf.capacity() / m_channels;
+    m_data->output_frames = float_out.capacity() / m_channels;
+    m_data->end_of_input = p_last;
+
+    src_process(m_state, m_data);
+    check_error();
+
+    std::vector<int16_t> out(m_data->output_frames_gen * m_channels);
+    src_float_to_short_array(float_out.data(), out.data(), out.capacity());
+
+    out.resize(out.capacity());
+    return out;
+}
---
+++ library/src/cpp/samplerate/resampler.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <vector>
+#include <samplerate.h>
+#include <cstdint>
+
+class resampler {
+    public:
+        /// @see http://www.mega-nerd.com/SRC/api_misc.html#Converters
+        enum class converter : int {
+            sinc_best = SRC_SINC_BEST_QUALITY,
+            sinc_medium = SRC_SINC_MEDIUM_QUALITY,
+            sinc_low = SRC_SINC_FASTEST,
+            zero_order_hold = SRC_ZERO_ORDER_HOLD,
+            linear = SRC_LINEAR
+        };
+
+        resampler(resampler::converter p_converter, int8_t p_channels, float p_ratio);
+        resampler(const resampler&);
+        ~resampler();
+
+        void ratio(float p_ratio);
+        float ratio();
+
+        std::vector<int16_t> process(std::vector<int16_t>::iterator p_begin,
+                                     std::vector<int16_t>::iterator p_end,
+                                     bool p_last = false);
+
+    private:
+        void check_error();
+
+        SRC_STATE* m_state;
+        SRC_DATA* m_data;
+        int m_error;
+        converter m_converter;
+        int8_t m_channels;
+};
