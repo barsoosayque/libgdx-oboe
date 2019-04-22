7f99eca0fce2469013ee22eb84ffbc064010532a
barsoosayque <shtoshich@gmail.com>
2019-04-22 14:11:37 +0700

    Send data from Audio Device to audio engine

--- library/src/cpp/native/oboeaudiodevice.cpp
+++ library/src/cpp/native/oboeaudiodevice.cpp
@@ -3,19 +3,27 @@
 #include "../utility/var.hpp"
 
 OBOEAUDIODEVICE_METHOD(jint, getLatency) (JNIEnv* env, jobject self) {
-
+    return shared_ptr_var<audio_engine>(env, self, "audioEngine")->payload_size();
 }
 
 OBOEAUDIODEVICE_METHOD(void, setVolume) (JNIEnv* env, jobject self, jfloat volume) {
     shared_ptr_var<audio_engine>(env, self, "audioEngine")->volume(volume);
 }
 
-OBOEAUDIODEVICE_METHOD(void, writeSamples) (JNIEnv* env, jobject self, jshortArray, jint, jint) {
+OBOEAUDIODEVICE_METHOD(void, writeSamples) (JNIEnv* env, jobject self, jshortArray data, jint offset, jint samples) {
+    auto engine = shared_ptr_var<audio_engine>(env, self, "audioEngine");
 
+    std::vector<int16_t> pcm(samples);
+    env->GetShortArrayRegion(data, offset, samples, pcm.data());
+    engine->play(pcm);
 }
 
-OBOEAUDIODEVICE_METHOD(void, writeSamples) (JNIEnv* env, jobject self, jfloatArray, jint, jint) {
+OBOEAUDIODEVICE_METHOD(void, writeSamples) (JNIEnv* env, jobject self, jfloatArray data, jint offset, jint samples) {
+    auto engine = shared_ptr_var<audio_engine>(env, self, "audioEngine");
 
+    std::vector<float> pcm(samples);
+    env->GetFloatArrayRegion(data, offset, samples, pcm.data());
+    engine->play(pcm);
 }
 
 OBOEAUDIODEVICE_METHOD(void, dispose) (JNIEnv* env, jobject self) {
