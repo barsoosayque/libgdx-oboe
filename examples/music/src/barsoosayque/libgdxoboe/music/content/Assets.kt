5e8363d41b0f85962fc17e080a8bdcca965e1241
barsoosayque <shtoshich@gmail.com>
2019-08-26 14:07:35 +0700

    Mono audio assets to test 1 channel tracks

--- examples/music/src/barsoosayque/libgdxoboe/music/content/Assets.kt
+++ examples/music/src/barsoosayque/libgdxoboe/music/content/Assets.kt
@@ -7,7 +7,8 @@ enum class SkinAsset(val path: String) { DEFAULT("uiskin.json") }
 enum class MusicAsset(val path: String, val duration: Float) {
     OEDO_CONTROLLER("oedo_controller.mp3", 255.6f),
     TWO_MOONS("two_moons.mp3", 7.75f),
-    PEACH_PIT("peach_pit.ogg", 287f);
+    PEACH_PIT("peach_pit.ogg", 287f),
+    PEACH_PIT_MONO("peach_pit_mono.ogg", 287f);
 
     fun get(assetManager: AssetManager): Music = assetManager[path]
     override fun toString(): String = "${super.toString()} ($path)"
--- examples/sounds/src/barsoosayque/libgdxoboe/sounds/content/Assets.kt
+++ examples/sounds/src/barsoosayque/libgdxoboe/sounds/content/Assets.kt
@@ -6,6 +6,9 @@ import com.badlogic.gdx.audio.Sound
 enum class SkinAsset(val path: String) { DEFAULT("uiskin.json") }
 enum class SoundAsset(val path: String) {
     AM("am.wav"),
+    AM_MONO_WAV("am_mono.wav"),
+    AM_MONO_OGG("am_mono.ogg"),
+    AM_MONO_MP3("am_mono.mp3"),
     TWO_MOONS("two_moons.mp3");
 
     fun get(assetManager: AssetManager): Sound = assetManager[path]
