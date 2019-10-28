e56ee37872d2ac21b87b57c8ea99e937b8285ee9
barsoosayque <shtoshich@gmail.com>
2019-10-28 16:01:50 +0700

    Optimized pan_effect

--- library/src/cpp/audio/pan_effect.hpp
+++ library/src/cpp/audio/pan_effect.hpp
@@ -8,15 +8,13 @@ class pan_effect {
         float pan() { return m_pan; }
         void pan(float p_pan) {
             m_pan = std::min(std::max(-1.0f, p_pan), 1.0f);
+            m_a = -std::max(m_pan, 0.0f);
+            m_b = std::min(m_pan, 0.0f);
         }
 
-        float modulation(int p_channel) {
-            switch (p_channel) {
-                case 0: return 1.0f - std::max(m_pan, .0f); break;
-                case 1: return 1.0f + std::min(m_pan, .0f); break;
-                default: return 1.0f; break;
-            }
+        inline float modulation(int p_channel) {
+            return 1 + m_a * (1 - p_channel) + m_b * p_channel;
         }
     private:
-        float m_pan;
+        float m_pan, m_a, m_b;
 };
