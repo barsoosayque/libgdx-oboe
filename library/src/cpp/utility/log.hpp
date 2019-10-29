52eb4a83336b1eecb831656c888c3987574a32a6
barsoosayque <shtoshich@gmail.com>
2019-10-29 13:13:24 +0700

    Warn log function

--- library/src/cpp/utility/log.hpp
+++ library/src/cpp/utility/log.hpp
@@ -5,14 +5,14 @@
 
 // Android logging utility
 // Using fmt for template parsing, so you can use either:
-// - printf style: debug("string: %s, number: %d", "ha", 3) => "[DEBUG] string: ha, number: 3"
-// - python style: info("string: {}, number: {}", "ha", 3) => "[INFO] string: ha, number: 3"
+// - printf style: debug("string: %s, number: %d", "ha", 3) => "D/libGDX-Oboe string: ha, number: 3"
+// - python style: info("string: {}, number: {}", "ha", 3) => "I/libGDX-Oboe string: ha, number: 3"
 
 // <><><><><><><><><><><><><><><><><><><><>
 //                Details
 // <><><><><><><><><><><><><><><><><><><><>
 
-enum class log_level { debug, info, error };
+enum class log_level { debug, info, error, warn };
 
 template<class ... Params>
 void log(log_level p_level, std::string_view p_template, Params... p_params) {
@@ -23,6 +23,7 @@ void log(log_level p_level, std::string_view p_template, Params... p_params) {
         case log_level::debug: level = ANDROID_LOG_DEBUG; break;
         case log_level::info: level = ANDROID_LOG_INFO; break;
         case log_level::error: level = ANDROID_LOG_ERROR; break;
+        case log_level::warn: level = ANDROID_LOG_WARN; break;
     }
 
     __android_log_print(level, "libGDX-Oboe", "%s", message.c_str());
@@ -45,4 +46,9 @@ inline void info(std::string_view p_template, Params... p_params) {
 template<class ... Params>
 inline void error(std::string_view p_template, Params... p_params) {
     log(log_level::error, p_template, p_params...);
+}
+
+template<class ... Params>
+inline void warn(std::string_view p_template, Params... p_params) {
+    log(log_level::warn, p_template, p_params...);
 }
