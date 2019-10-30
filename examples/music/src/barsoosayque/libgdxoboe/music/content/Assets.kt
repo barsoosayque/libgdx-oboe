5eb08e33a0d51f5407d47ab58430e22b3a6a2f34
barsoosayque <shtoshich@gmail.com>
2019-10-30 12:36:07 +0700

    Add smol beat asset to test music repeat

--- examples/music/src/barsoosayque/libgdxoboe/music/content/Assets.kt
+++ examples/music/src/barsoosayque/libgdxoboe/music/content/Assets.kt
@@ -8,7 +8,8 @@ enum class MusicAsset(val path: String, val duration: Float) {
     OEDO_CONTROLLER("oedo_controller.mp3", 255.6f),
     TWO_MOONS("two_moons.mp3", 7.75f),
     PEACH_PIT("peach_pit.ogg", 287f),
-    PEACH_PIT_MONO("peach_pit_mono.ogg", 287f);
+    PEACH_PIT_MONO("peach_pit_mono.ogg", 287f),
+    BEAT("beat.ogg", 1.85f);
 
     fun get(assetManager: AssetManager): Music = assetManager[path]
     override fun toString(): String = "${super.toString()} ($path)"
