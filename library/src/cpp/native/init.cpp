c4d18ec8f5e9612c63fff0e9dbcf42d0fa9b7198
barsoosayque <shtoshich@gmail.com>
2020-01-13 18:18:43 +0700

    Remove unused jni code

--- library/src/cpp/native/init.cpp
+++ library/src/cpp/native/init.cpp
@@ -1,24 +1,12 @@
 #include "../jni/jni_context.hpp"
 #include "../jni/jvm_class.hpp"
-#include "../jni/jvm_definitions.hpp"
 
 JavaVM* jni_context::s_jvm = 0;
 jvm_class::class_cache_map jvm_class::g_class_cache;
 jvm_class::field_cache_map jvm_class::g_field_cache;
 jvm_class::method_cache_map jvm_class::g_method_cache;
+
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* jvm, void *reserved) {
     jni_context::init(jvm);
-
-    // jvm caching
-    jvm_class::cache_class("barsoosayque/libgdxoboe/AudioDecoder");
-    jvm_class::cache_class("barsoosayque/libgdxoboe/AudioDecoder$Pcm");
-    jvm_class::cache_field<int>("barsoosayque/libgdxoboe/AudioDecoder$Pcm", "size");
-    jvm_class::cache_field<ByteBuffer>("barsoosayque/libgdxoboe/AudioDecoder$Pcm", "data");
-    jvm_class::cache_field<bool>("barsoosayque/libgdxoboe/AudioDecoder$Pcm", "isLast");
-    jvm_class::cache_method<void(AssetFileDescriptor)>("barsoosayque/libgdxoboe/AudioDecoder", "<init>");
-    jvm_class::cache_method<Pcm(int)>("barsoosayque/libgdxoboe/AudioDecoder", "decode");
-    jvm_class::cache_method<Pcm()>("barsoosayque/libgdxoboe/AudioDecoder", "decode");
-    jvm_class::cache_method<void(float)>("barsoosayque/libgdxoboe/AudioDecoder", "seek");
-
     return JNI_VERSION_1_6;
 }
