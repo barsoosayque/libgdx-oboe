92f48d099b3d8aea0ab25582c5db26ab264e57b4
barsoosayque <shtoshich@gmail.com>
2020-02-20 12:20:11 +0700

    Two moons for either of formats

--- examples/music/src/barsoosayque/libgdxoboe/music/content/Assets.kt
+++ examples/music/src/barsoosayque/libgdxoboe/music/content/Assets.kt
@@ -6,7 +6,9 @@ import com.badlogic.gdx.audio.Music
 enum class SkinAsset(val path: String) { DEFAULT("uiskin.json") }
 enum class MusicAsset(val path: String, val duration: Float) {
     OEDO_CONTROLLER("oedo_controller.mp3", 255.6f),
-    TWO_MOONS("two_moons.mp3", 7.75f),
+    TWO_MOONS_MP3("two_moons.mp3", 7.75f),
+    TWO_MOONS_OGG("two_moons.ogg", 7.75f),
+    TWO_MOONS_WAV("two_moons.wav", 7.75f),
     PEACH_PIT("peach_pit.ogg", 287f),
     PEACH_PIT_MONO("peach_pit_mono.ogg", 287f),
     BEAT("beat.ogg", 1.85f);
