f4cbeac95d1ca2f1e7e204396885ae22c34efbaa
barsoosayque <shtoshich@gmail.com>
2021-09-29 20:05:16 +0700

    fix sounds example and add per-sound controls

--- examples/sounds/src/barsoosayque/libgdxoboe/sounds/app/AppUi.kt
+++ examples/sounds/src/barsoosayque/libgdxoboe/sounds/app/AppUi.kt
@@ -2,33 +2,35 @@ package barsoosayque.libgdxoboe.sounds.app
 
 import barsoosayque.libgdxoboe.sounds.content.SoundAsset
 import com.badlogic.gdx.assets.AssetManager
-import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox
 import com.badlogic.gdx.scenes.scene2d.ui.Slider
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup
 import com.badlogic.gdx.utils.viewport.ExtendViewport
 import ktx.actors.onChange
-import ktx.actors.onChangeEvent
 import ktx.scene2d.*
+import kotlin.concurrent.thread
 import kotlin.math.pow
 import com.badlogic.gdx.utils.Array as GdxArray
 
 class AppUi(
-        sounds: Array<SoundAsset>,
-        assetManager: AssetManager
+    sounds: Array<SoundAsset>,
+    private val assetManager: AssetManager
 ) : Stage(ExtendViewport(480f, 700f)) {
-    private lateinit var loopCheckBox: CheckBox
-    private lateinit var volumeSlider: Slider
-    private lateinit var panSlider: Slider
-    private lateinit var pitchSlider: Slider
+    private var loopCheckBox: CheckBox
+    private var volumeSlider: Slider
+    private var panSlider: Slider
+    private var pitchSlider: Slider
+    private var soundsGrp: VerticalGroup
     private val volume: Float get() = volumeSlider.value
     private val pan: Float get() = panSlider.value
     private val pitch: Float get() = 2f.pow(pitchSlider.value)
     private val isLooping: Boolean get() = loopCheckBox.isChecked
+    private val playingSounds = mutableListOf<Pair<SoundAsset, Long>>()
 
-    private var selectedSound: Sound = sounds.first().get(assetManager)
+    private var selectedSoundAsset: SoundAsset = sounds.first()
 
-    val root = table {
+    val root = scene2d.table {
         setFillParent(true)
         pad(30f)
         top()
@@ -37,25 +39,32 @@ class AppUi(
         verticalGroup {
             center()
             space(30f)
-            verticalGroup {
-                label("Select one of the sounds:")
-                selectBoxOf<SoundAsset>(GdxArray(sounds)).onChangeEvent { _, selectBox ->
-                    selectedSound = selectBox.selected.get(assetManager)
+            horizontalGroup {
+                space(30f)
+                verticalGroup {
+                    label("Select one of the sounds:")
+                    selectBoxOf<SoundAsset>(GdxArray(sounds)).onChange {
+                        selectedSoundAsset = selected
+                    }
+                }
+                textButton("Play").onChange {
+                    val selectedSound = selectedSoundAsset.get(assetManager)
+                    val id = if (isLooping) selectedSound.loop(volume, pitch, pan)
+                    else selectedSound.play(volume, pitch, pan)
+
+                    playingSounds.add(selectedSoundAsset to id)
+                    if (!isLooping) {
+                        thread {
+                            Thread.sleep((selectedSoundAsset.duration * 1000).toLong())
+                            playingSounds.removeAll { (_, soundId) -> soundId == id }
+                            updateSounds()
+                        }
+                    }
+                    updateSounds()
                 }
             }
             table {
                 defaults().pad(10f)
-                label("Soundpool controls: ")
-                horizontalGroup {
-                    space(10f)
-                    textButton("Play").onChange {
-                        if (isLooping) selectedSound.loop(volume, pitch, pan)
-                        else selectedSound.play(volume, pitch, pan)
-                    }
-                    textButton("Resume").onChange { selectedSound.resume() }
-                    textButton("Pause").onChange { selectedSound.pause() }
-                    textButton("Stop").onChange { selectedSound.stop() }
-                }
                 row()
                 label("Volume: ")
                 volumeSlider = slider { value = 1.0f }
@@ -69,6 +78,47 @@ class AppUi(
                 label("Pitch: ")
                 pitchSlider = slider(-1f) { value = 0.0f }
             }
+            verticalGroup {
+                space(10f)
+                label("Current sounds")
+                horizontalGroup {
+                    space(10f)
+                    textButton("Resume all").onChange {
+                        sounds.forEach { it.get(assetManager).resume() }
+                    }
+                    textButton("Pause all").onChange {
+                        sounds.forEach {
+                            it.get(assetManager).pause()
+                        }
+                    }
+                    textButton("Stop all").onChange {
+                        sounds.forEach { it.get(assetManager).stop() }
+                        playingSounds.clear()
+                        updateSounds()
+                    }
+                }
+                soundsGrp = verticalGroup {
+                    space(10f)
+                }
+            }
+
         }.cell(grow = true)
     }.let(::addActor)
+
+    private fun updateSounds() {
+        soundsGrp.clear()
+        playingSounds.forEach { (asset, id) ->
+            soundsGrp.addActor(scene2d.horizontalGroup {
+                space(10f)
+                label("${asset.name} (${id})")
+                textButton("Resume").onChange { asset.get(assetManager).resume(id) }
+                textButton("Pause").onChange { asset.get(assetManager).pause(id) }
+                textButton("Stop").onChange {
+                    asset.get(assetManager).stop(id)
+                    playingSounds.removeAll { (_, soundId) -> soundId == id }
+                    updateSounds()
+                }
+            })
+        }
+    }
 }
--- examples/sounds/src/barsoosayque/libgdxoboe/sounds/content/Assets.kt
+++ examples/sounds/src/barsoosayque/libgdxoboe/sounds/content/Assets.kt
@@ -4,12 +4,12 @@ import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.audio.Sound
 
 enum class SkinAsset(val path: String) { DEFAULT("uiskin.json") }
-enum class SoundAsset(val path: String) {
-    AM("am.wav"),
-    AM_MONO_WAV("am_mono.wav"),
-    AM_MONO_OGG("am_mono.ogg"),
-    AM_MONO_MP3("am_mono.mp3"),
-    TWO_MOONS("two_moons.mp3");
+enum class SoundAsset(val path: String, val duration: Float) {
+    AM("am.wav", 0.39f),
+    AM_MONO_WAV("am_mono.wav", 0.39f),
+    AM_MONO_OGG("am_mono.ogg", 0.39f),
+    AM_MONO_MP3("am_mono.mp3", 0.39f),
+    TWO_MOONS("two_moons.mp3", 7.70f);
 
     fun get(assetManager: AssetManager): Sound = assetManager[path]
     override fun toString(): String = "${super.toString()} ($path)"
